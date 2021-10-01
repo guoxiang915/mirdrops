c0c0b62b563f782fa9a0b7b5d908c465d32c59ea
guoxiang915 <guoxiang915@hotmail.com>
2021-10-01 12:43:12 -0400

    Fix dependency & remove legacy codes

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,8 +1,6 @@
 import routes from "../routes"
 import Container from "../components/Container"
 import { ContractProvider, useContractState } from "../hooks/useContract"
-import { StatsProvider, useStatsState } from "../statistics/useStats"
-import useConnectGraph from "../hooks/useConnectGraph"
 import useAddress from "../hooks/useAddress"
 import Header from "./Header"
 import "./App.scss"
@@ -12,19 +10,15 @@ import Footer from "./Footer"
 const App = () => {
   const address = useAddress()
   const contract = useContractState(address)
-  const stats = useStatsState()
   const claim = useClaimState()
-  useConnectGraph(address)
 
   return (
     <ContractProvider value={contract}>
-      <StatsProvider value={stats}>
-        <ClaimProvider value={claim}>
-          <Header />
-          <Container>{routes()}</Container>
-          <Footer />
-        </ClaimProvider>
-      </StatsProvider>
+      <ClaimProvider value={claim}>
+        <Header />
+        <Container>{routes()}</Container>
+        <Footer />
+      </ClaimProvider>
     </ContractProvider>
   )
 }
